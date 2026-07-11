12e5f5cd88c4a5709f537715a1dc40646e8a9570
Sakshi Wanare <sakshiwanare@Sakshis-MacBook-Air.local>
2026-07-11 20:36:59 +0530

    Added mobile field for registration at customer app

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,9 @@ const jwtSecret = process.env.JWT_SECRET || 'fallback_secret_dev';
 
 exports.registerUser = async (req, res) => {
   try {
-    const { name, email, password,role } = req.body;
+    const { name, email, mobile, password,role } = req.body;
 
-    if (!name || !email || !password) {
+    if (!name || !email || !mobile || !password) {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
@@ -16,12 +16,20 @@ exports.registerUser = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
+    const mobileExists = await User.findOne({ mobile });
+    if (mobileExists) {
+      return res.status(400).json({
+        message: 'Mobile number already exists'
+      });
+    }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({
       name,
       email,
+      mobile,
       password: hashedPassword,
       role: role || 'CUSTOMER',
     });
